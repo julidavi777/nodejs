e834d36b974303099b334586a901e45062e68c06
julidavi777 <anturi8@misena.edu.co>
2023-05-16 11:36:31 -0500

    documents can be created on DB (89)

---
+++ controllers/tourController.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const Tour = require('./../models/tourModel');
+
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime
+    /*     results: tours.length,
+    data: {
+      tours
+    } */
+  });
+};
+
+exports.getTour = (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+
+  /*   const tour = tours.find(el => el.id === id); */
+
+  /*  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  }); */
+};
+
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'sucess',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid data sent!'
+    });
+  }
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+};
