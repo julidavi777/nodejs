ed81524157c460f31173127f0f1282a564543457
julidavi777 <anturi8@misena.edu.co>
2023-05-16 12:09:09 -0500

    Reading documents (90)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,33 +1,6 @@
 const fs = require('fs');
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime
-    /*     results: tours.length,
-    data: {
-      tours
-    } */
-  });
-};
-
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-
-  /*   const tour = tours.find(el => el.id === id); */
-
-  /*  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  }); */
-};
-
 exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body);
@@ -44,6 +17,46 @@ exports.createTour = async (req, res) => {
     });
   }
 };
+
+exports.getAllTours = async (req, res) => {
+  try{
+
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+  }catch(err){
+    res.status(404).json({
+      status: "fail",
+      message: err, 
+    }); 
+  }
+  };
+
+exports.getTour = async (req, res) => {
+  try{
+   const tour =  await Tour.findById(req.params.id)
+    res.status(200).json(
+      {
+        status: "success",
+        data: {
+          tour
+        }
+      })
+  }catch(err){
+    res.status(404).json({
+      status: "fail",
+      message: err
+    })
+  }
+ 
+};
+
+
 
 exports.updateTour = (req, res) => {
   res.status(200).json({
