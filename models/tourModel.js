42faaf5b8900c4c4ecbf57666070e4f8820a2da7
julidavi777 <anturi8@misena.edu.co>
2023-05-17 13:47:36 -0500

    tour controler shows specifics errors and tourModel has new conditions (93)

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,17 +5,41 @@ const tourSchema = new mongoose.Schema({
     name: {
       type: String,
       required: [true, 'A tour must have a name'],
-      unique: true
+      unique: true,
+      trim: true
+
     },
-    rating:{
+    ratingsAverage:{
       type: Number,
-      default: 4.5
+      default: 4.5 
     },
+
     price:{ 
       type: Number, 
       require: [true, 'A tour must have a price']
-    }
+    },
+    priceDiscount: Number, 
+    summary: {
+      type: String, 
+      trim: true,
+      required: [true, 'A tour must have a description']
+    },
+    imageCover: {
+      type: String, 
+      required: [true, 'A tour must have a cover image']
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    startDates: [Date]
   })
+
   tourSchema.pre('save', function(next){
     console.log('data added to database');
     next(); 
