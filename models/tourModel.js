0119b66bf617427211b1ceaaac525db7eb7688f3
julidavi777 <anturi8@misena.edu.co>
2023-05-23 07:31:47 -0500

    query middleware added (106)

--- models/tourModel.js
+++ models/tourModel.js
@@ -84,6 +84,23 @@ tourSchema.post('save', function(doc, next){
   console.log(doc);
   next(); 
 }); */
+
+//QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function(next){
+  this.find({ secretTour: {$ne: true}});
+  this.start = Date.now(); 
+  next(); 
+})
+/* tourSchema.pre('findOne', function(next){
+  this.find({ secretTour: {$ne: true}});
+  next(); 
+}) */
+
+tourSchema.post(/^find/, function(docs, next){
+  console.log(`Query took ${Date.now() - this.start} millisencods!`);
+  console.log(docs);
+  next(); 
+})
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
