4966eb486a26fddd2ab0a1abc2fe8cb8cbcbbe76
julidavi777 <anturi8@misena.edu.co>
2023-05-23 08:22:16 -0500

    validations added (108).

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,9 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
-      trim: true
+      trim: true,
+      maxLength: [40, 'A tour name must have less than 40 characters'],
+      minLength: [10, 'A tour name must have more than 10 characters']
     },
     slug: String, 
     duration: {
@@ -21,11 +23,18 @@ const tourSchema = new mongoose.Schema(
 
     difficulty: {
       type: String,
-      Required: [true, 'A tour must have a difficulty ']
+      Required: [true, 'A tour must have a difficulty '],
+      enum: {
+        values: ['easy','medium','difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
+
+      } 
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5, 
+      min: [1, 'Rating must be above 1.0'], 
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
