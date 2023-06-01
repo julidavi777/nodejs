61d4259c5ec907b929db2155f757e639d306cbb5
julidavi777 <anturi8@misena.edu.co>
2023-06-01 12:20:12 -0500

    cookie added as response.

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,12 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res)=>{
   const token = signToken(user._id);
-  
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 *60 *60 * 1000),
+    httpsOnly: true
+  }
+  if(process.env.NODE_ENV == 'production') cookieOptions.secure = true; 
+  res.cookie('jwt', token,cookieOptions)
   res.status(statusCode).json({
     status: 'success',
     token: token,
@@ -23,6 +28,7 @@ const createSendToken = (user, statusCode, res)=>{
     }
   });
 }
+
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
